ca5171dcb94c8143e05769cab9ba42269cebf330
Evgeniy Fedorenko <evgeny.fedorenko@aliexpress.ru>
2025-03-23 17:43:40 +0300

    sprint 3

--- app/src/main/java/ru/yandex/architectureproject/presentation/viewmodel/TaskViewModel.kt
+++ app/src/main/java/ru/yandex/architectureproject/presentation/viewmodel/TaskViewModel.kt
@@ -3,12 +3,14 @@ package ru.yandex.architectureproject.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.yandex.architectureproject.domain.AddTaskUseCase
@@ -18,6 +20,7 @@ import ru.yandex.architectureproject.domain.GetAllTasksUseCase
 import ru.yandex.architectureproject.domain.IncompleteTaskUseCase
 import ru.yandex.architectureproject.presentation.state.TaskAction
 import ru.yandex.architectureproject.presentation.state.TaskState
+import java.util.concurrent.ConcurrentHashMap
 
 class TaskViewModel(
     private val addTaskUseCase: AddTaskUseCase,
@@ -27,6 +30,8 @@ class TaskViewModel(
     private val incompleteTaskUseCase: IncompleteTaskUseCase,
     private val ioDispatcher: CoroutineDispatcher,
 ) : ViewModel() {
+    private val taskForDeletionJobMap = ConcurrentHashMap<Int, Job>()
+
     private val _state = MutableStateFlow<TaskState>(TaskState.Loading)
     val state: StateFlow<TaskState> = _state.asStateFlow()
 
@@ -35,7 +40,24 @@ class TaskViewModel(
     }
 
     fun reduce(action: TaskAction) {
-        // TODO: Здесь должна быть обработка действий
+        viewModelScope.launch {
+            when (action) {
+                is TaskAction.LoadTasks -> loadTasks()
+                is TaskAction.AddTask -> addTaskUseCase(action.task)
+
+                is TaskAction.UpdateTaskStatus -> {
+                    if (action.isDone) {
+                        taskForDeletionJobMap[action.taskId] = this.coroutineContext.job
+                        completeTaskUseCase(action.taskId)
+                    } else {
+                        taskForDeletionJobMap[action.taskId]?.cancel()
+                        incompleteTaskUseCase(action.taskId)
+                    }
+                }
+
+                is TaskAction.DeleteTask -> deleteTaskUseCase(action.taskId)
+            }
+        }
     }
 
     private suspend fun loadTasks() {
