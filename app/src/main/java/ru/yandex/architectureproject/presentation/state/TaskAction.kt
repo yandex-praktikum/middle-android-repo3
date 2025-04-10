f7c0a1cb938db7166672600923ddb5d7c0ab230d
auron77777 <auron77777@gmail.com>
2025-04-10 23:18:37 +0300

    ver 0

--- app/src/main/java/ru/yandex/architectureproject/presentation/state/TaskAction.kt
+++ app/src/main/java/ru/yandex/architectureproject/presentation/state/TaskAction.kt
@@ -1,5 +1,7 @@
 package ru.yandex.architectureproject.presentation.state
-
+//////
 sealed class TaskAction {
+
+
     // TODO: Здесь должны быть действия (загрузка заданий, добавление задания, обновление статуса задания (сделано/не сделано) и удаление задания)
 }
